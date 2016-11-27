ac17b174d55dd69c25ecc314f95e2eaff6cce585
OliverMega <howarrdyang1991@gmail.com>
2016-11-27 16:07:56 +0800

    add Solution15

---
+++ hackerrank-java/src/Solution15.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * Java substring
+ * 
+ * @author HowardYang
+ *
+ */
+public class Solution15 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        
+        System.out.println(S.substring(start, end));
+        
+        in.close();
+	}
+
+}
